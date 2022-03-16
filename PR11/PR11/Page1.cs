356622e31652961706a7371d9a277e67f4e622db
Nekrasov Zahar <pitard671@gmail.com>
2022-03-16 23:49:17 +0300

    Добавьте файлы проекта.

---
+++ PR11/PR11/Page1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PR11
+{
+    public class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Nekrasov Zahar PKS-30K, horoshiy den chtobi v  chas nochi sidet izuchat xamarinforms" }
+                }
+            };
+        }
+    }
+}
